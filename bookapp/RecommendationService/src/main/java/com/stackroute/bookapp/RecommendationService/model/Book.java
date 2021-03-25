ce590b403cf584c1a20401c6f044bec1c2775fe6
Laribok.Syiemlieh <laribok.syiemlieh@ibm.com>
2021-03-25 20:00:44 +0530

    refactored Book model

--- bookapp/RecommendationService/src/main/java/com/stackroute/bookapp/RecommendationService/model/Book.java
+++ bookapp/RecommendationService/src/main/java/com/stackroute/bookapp/RecommendationService/model/Book.java
@@ -1,25 +1,9 @@
 package com.stackroute.bookapp.RecommendationService.model;
 
-import java.util.Date;
-import java.util.List;
-
 public class Book {
 	private String id;
 	private String kind;
-	private String title;
-	private List<String> authors;
-	private String publisher;
-	private Date publishedDate;
-	private String description;
-	private String isbn13;
-	private String isbn10;
-	private String pageCount;
-	private List<String> categories;
-	private String averageRating;
-	private int ratingsCount;
-	private String smallThumbnail;
-	private String thumbnail;
-	private String language;
+	private VolumeInfo volumeInfo;
 	
 	public Book() {
 		super();
@@ -41,118 +25,12 @@ public class Book {
 		this.kind = kind;
 	}
 
-	public String getTitle() {
-		return title;
+	public VolumeInfo getVolumeInfo() {
+		return volumeInfo;
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
+	public void setVolumeInfo(VolumeInfo volumeInfo) {
+		this.volumeInfo = volumeInfo;
 	}
-
-	public List<String> getAuthors() {
-		return authors;
-	}
-
-	public void setAuthors(List<String> authors) {
-		this.authors = authors;
-	}
-
-	public String getPublisher() {
-		return publisher;
-	}
-
-	public void setPublisher(String publisher) {
-		this.publisher = publisher;
-	}
-
-	public Date getPublishedDate() {
-		return publishedDate;
-	}
-
-	public void setPublishedDate(Date publishedDate) {
-		this.publishedDate = publishedDate;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getIsbn13() {
-		return isbn13;
-	}
-
-	public void setIsbn13(String isbn13) {
-		this.isbn13 = isbn13;
-	}
-
-	public String getIsbn10() {
-		return isbn10;
-	}
-
-	public void setIsbn10(String isbn10) {
-		this.isbn10 = isbn10;
-	}
-
-	public String getPageCount() {
-		return pageCount;
-	}
-
-	public void setPageCount(String pageCount) {
-		this.pageCount = pageCount;
-	}
-
-	public List<String> getCategories() {
-		return categories;
-	}
-
-	public void setCategories(List<String> categories) {
-		this.categories = categories;
-	}
-
-	public String getAverageRating() {
-		return averageRating;
-	}
-
-	public void setAverageRating(String averageRating) {
-		this.averageRating = averageRating;
-	}
-
-	public int getRatingsCount() {
-		return ratingsCount;
-	}
-
-	public void setRatingsCount(int ratingsCount) {
-		this.ratingsCount = ratingsCount;
-	}
-
-	public String getSmallThumbnail() {
-		return smallThumbnail;
-	}
-
-	public void setSmallThumbnail(String smallThumbnail) {
-		this.smallThumbnail = smallThumbnail;
-	}
-
-	public String getThumbnail() {
-		return thumbnail;
-	}
-
-	public void setThumbnail(String thumbnail) {
-		this.thumbnail = thumbnail;
-	}
-
-	public String getLanguage() {
-		return language;
-	}
-
-	public void setLanguage(String language) {
-		this.language = language;
-	}
-	
-	
 	
 }
---
+++ bookapp/RecommendationService/src/main/java/com/stackroute/bookapp/RecommendationService/model/VolumeInfo.java
@@ -0,0 +1,130 @@
+package com.stackroute.bookapp.RecommendationService.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class VolumeInfo {
+	private String title;
+	private List<String> authors;
+	private String publisher;
+	private Date publishedDate;
+	private String description;
+	private List<IndustryIdentifier> industryIdentifiers;
+	private String pageCount;
+	private List<String> categories;
+	private String averageRating;
+	private int ratingsCount;
+	private ImageLinks imageLinks;
+	private String previewLink;
+	private String language;
+	
+	public VolumeInfo() {
+		super();
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<String> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(List<String> authors) {
+		this.authors = authors;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public Date getPublishedDate() {
+		return publishedDate;
+	}
+
+	public void setPublishedDate(Date publishedDate) {
+		this.publishedDate = publishedDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<IndustryIdentifier> getIndustryIdentifiers() {
+		return industryIdentifiers;
+	}
+
+	public void setIndustryIdentifiers(List<IndustryIdentifier> industryIdentifiers) {
+		this.industryIdentifiers = industryIdentifiers;
+	}
+
+	public String getPageCount() {
+		return pageCount;
+	}
+
+	public void setPageCount(String pageCount) {
+		this.pageCount = pageCount;
+	}
+
+	public List<String> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<String> categories) {
+		this.categories = categories;
+	}
+
+	public String getAverageRating() {
+		return averageRating;
+	}
+
+	public void setAverageRating(String averageRating) {
+		this.averageRating = averageRating;
+	}
+
+	public int getRatingsCount() {
+		return ratingsCount;
+	}
+
+	public void setRatingsCount(int ratingsCount) {
+		this.ratingsCount = ratingsCount;
+	}
+
+	public ImageLinks getImageLinks() {
+		return imageLinks;
+	}
+
+	public void setImageLinks(ImageLinks imageLinks) {
+		this.imageLinks = imageLinks;
+	}
+
+	public String getPreviewLink() {
+		return previewLink;
+	}
+
+	public void setPreviewLink(String previewLink) {
+		this.previewLink = previewLink;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	
+	
+}
