5c1bfcd456b9841faef8111860b47283e2aff89c
Rajdeep Deb <rajdeep.deb@ibm.com>
2021-03-26 20:21:46 +0530

    added jwtfilter tofavservice

--- bookapp/FavouriteService/src/main/java/com/stackroute/bookapp/FavouriteService/FavouriteServiceApplication.java
+++ bookapp/FavouriteService/src/main/java/com/stackroute/bookapp/FavouriteService/FavouriteServiceApplication.java
@@ -1,13 +1,32 @@
 package com.stackroute.bookapp.FavouriteService;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.stackroute.bookapp.FavouriteService.config.JWTFilter;
 
 @SpringBootApplication
 public class FavouriteServiceApplication {
+	
+	@Bean
+	public FilterRegistrationBean<JWTFilter> filterRegistrationBean(){
+		FilterRegistrationBean<JWTFilter> filter = new FilterRegistrationBean<>();
+		filter.addUrlPatterns("/");
+		filter.setFilter(new JWTFilter());
+		return filter;
+		
+		
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FavouriteServiceApplication.class, args);
+		System.out.println("hello from localhost://8082");
 	}
+	
+
 
 }
