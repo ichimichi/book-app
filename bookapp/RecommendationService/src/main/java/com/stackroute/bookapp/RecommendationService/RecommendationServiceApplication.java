ddeefba5b78ea8ea5cbb515147b4d49477717289
Laribok.Syiemlieh <laribok.syiemlieh@ibm.com>
2021-03-26 19:55:55 +0530

    added jwt filter to recommendation service

--- bookapp/RecommendationService/src/main/java/com/stackroute/bookapp/RecommendationService/RecommendationServiceApplication.java
+++ bookapp/RecommendationService/src/main/java/com/stackroute/bookapp/RecommendationService/RecommendationServiceApplication.java
@@ -2,9 +2,21 @@ package com.stackroute.bookapp.RecommendationService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.stackroute.bookapp.RecommendationService.jwtFilter.JwtFilter;
 
 @SpringBootApplication
 public class RecommendationServiceApplication {
+
+	@Bean
+	public FilterRegistrationBean<JwtFilter> jwtFilter() {
+		FilterRegistrationBean<JwtFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+		filterRegistrationBean.addUrlPatterns("/api/v1/recommendation");
+		filterRegistrationBean.setFilter(new JwtFilter());
+		return filterRegistrationBean;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecommendationServiceApplication.class, args);
