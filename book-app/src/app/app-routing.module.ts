3efa6459ab9f174e2b748b12ee96643692f3070a
Laribok.Syiemlieh <laribok.syiemlieh@ibm.com>
2021-03-27 13:05:02 +0530

    basic routing implemented

--- book-app/src/app/app-routing.module.ts
+++ book-app/src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SearchResultComponent } from './components/search-result/search-result.component';
+import { SearchComponent } from './components/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'search',
+        component: SearchComponent,
+      },
+      {
+        path: 'searchresult',
+        component: SearchResultComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
