ddc3908606a7ce935f0d1e2586e1a382b4c9f562
Rajdeep Deb <rajdeep.deb@ibm.com>
2021-03-29 01:16:34 +0530

    Added register component and FavoriteService basic

--- book-app/src/app/services/favourite.service.ts
+++ book-app/src/app/services/favourite.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Book } from '../models/book';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavouriteService {
 
-  constructor() { }
+    api_endpoint="http://localhost:8082/api/v1/fav/?userId=rd125";
+    constructor(private httpClient:HttpClient){}
+    getfavs(){
+      return this.httpClient.get<Book[]>(this.api_endpoint);
+    }
+    // errhandler(error:HttpErrorResponse){
+    //   return Observable.throw(error.message || 'Server Error');
+      
+    // }
 }
