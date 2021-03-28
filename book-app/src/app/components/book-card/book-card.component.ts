db14850a5645d2908a245f546d8bd68fcfc4500e
Laribok.Syiemlieh <laribok.syiemlieh@ibm.com>
2021-03-28 15:48:03 +0530

    implemented search result component

--- book-app/src/app/components/book-card/book-card.component.ts
+++ book-app/src/app/components/book-card/book-card.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Book } from 'src/app/models/book';
+import { ImageLinks } from 'src/app/models/image-links';
 
 @Component({
   selector: 'app-book-card',
   templateUrl: './book-card.component.html',
-  styleUrls: ['./book-card.component.css']
+  styleUrls: ['./book-card.component.css'],
 })
 export class BookCardComponent implements OnInit {
+  @Input() book: Book | undefined;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  getThumbnail(imageLinks: ImageLinks) {
+    if (imageLinks) {
+      return imageLinks.thumbnail;
+    } else {
+      return '../../../assets/books-icon.svg';
+    }
   }
-
 }
--- book-app/src/app/components/search-result/search-result.component.ts
+++ book-app/src/app/components/search-result/search-result.component.ts
@@ -1,15 +1,43 @@
+import { utf8Encode } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Book } from 'src/app/models/book';
+import { ImageLinks } from 'src/app/models/image-links';
+import { GoogleBooksService } from 'src/app/services/google-books.service';
 
 @Component({
   selector: 'app-search-result',
   templateUrl: './search-result.component.html',
-  styleUrls: ['./search-result.component.css']
+  styleUrls: ['./search-result.component.css'],
 })
 export class SearchResultComponent implements OnInit {
+  bookList: Array<Book> = [];
 
-  constructor() { }
+  constructor(
+    private googleBooksService: GoogleBooksService,
+    private activateRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.activateRoute.queryParams.subscribe((params) => {
+      console.log(params.searchQuery);
+      this.googleBooksService.search(params.searchQuery).subscribe(
+        (res: any) => {
+          console.log(res.items);
+          this.bookList = res.items;
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
+    });
   }
 
+  getThumbnail(imageLinks: ImageLinks) {
+    if (imageLinks) {
+      return imageLinks.thumbnail;
+    } else {
+      return '../../../assets/books-icon.svg';
+    }
+  }
 }
