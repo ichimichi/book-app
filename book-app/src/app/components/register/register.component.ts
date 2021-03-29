d54ca0c87c01e13ca5189e1eefb91c402251d0fd
Laribok.Syiemlieh <laribok.syiemlieh@ibm.com>
2021-03-29 22:31:18 +0530

    removed unused imports and dependencies, added more media query for search result

--- book-app/src/app/components/register/register.component.ts
+++ book-app/src/app/components/register/register.component.ts
@@ -1,52 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import * as moment from 'moment';
 import { User } from 'src/app/models/user';
 import { UserAuthenticationService } from 'src/app/services/user-authentication.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
-  registerForm=new FormGroup({
+  registerForm = new FormGroup({
     email: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required]),
-    name:new FormControl('', [Validators.required]),
-    date:new FormControl('', [Validators.required]),
-
-
+    name: new FormControl('', [Validators.required]),
+    date: new FormControl('', [Validators.required]),
   });
 
   constructor(
     private userAuthService: UserAuthenticationService,
     private router: Router
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  register(){
-
-
-    if (this.registerForm.valid) {  
+  register() {
+    if (this.registerForm.valid) {
       console.log(this.registerForm.value);
-      const dt=new Date(this.registerForm.value.date);
-      const formdt=moment(dt).format("YYYY/MM/DD");
 
-      let user:User={
-        name:this.registerForm.value.name,
-        email:this.registerForm.value.name,
-        password:this.registerForm.value.password,
-        dob:this.registerForm.value.date
+      let user: User = {
+        name: this.registerForm.value.name,
+        email: this.registerForm.value.email,
+        password: this.registerForm.value.password,
+        dob: this.registerForm.value.date,
+      };
 
-
-      }
-      // let user=
-      // this.userAuthService.register()
       this.userAuthService.register(user);
     } else {
       alert('Something went wrong');
