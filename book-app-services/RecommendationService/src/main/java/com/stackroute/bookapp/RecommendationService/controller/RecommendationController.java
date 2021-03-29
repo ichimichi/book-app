a331463fc7bcf188666b90705256ebb5c19469a9
Laribok.Syiemlieh <laribok.syiemlieh@ibm.com>
2021-03-29 15:18:37 +0530

    removed userId as request param and obtain from jwt claims instead

--- book-app-services/RecommendationService/src/main/java/com/stackroute/bookapp/RecommendationService/controller/RecommendationController.java
+++ book-app-services/RecommendationService/src/main/java/com/stackroute/bookapp/RecommendationService/controller/RecommendationController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.stackroute.bookapp.RecommendationService.exception.BookAlreadyExistsException;
 import com.stackroute.bookapp.RecommendationService.model.Book;
 import com.stackroute.bookapp.RecommendationService.service.RecommendationServiceImpl;
+
+import io.jsonwebtoken.Claims;
 
 @RestController
 @CrossOrigin("http://localhost:4200")
@@ -32,27 +35,30 @@ public class RecommendationController {
 	}
 	
 	@GetMapping
-	public ResponseEntity<?> getAllRecommendedBooks(@RequestParam String userId){
+	public ResponseEntity<?> getAllRecommendedBooks(@RequestAttribute Claims claims){
+		System.out.println(claims.getId());
 		try {
-			return new ResponseEntity<List<Book>>(recommendationService.getAllRecommendedBooks(userId), HttpStatus.OK);
+			return new ResponseEntity<List<Book>>(recommendationService.getAllRecommendedBooks(claims.getId()), HttpStatus.OK);
 		}catch (Exception e) {
 			return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
 		}
 	}
 	
 	@PostMapping
-	public ResponseEntity<?> addtoRecommendations(@RequestParam String userId, @RequestBody Book book){
+	public ResponseEntity<?> addtoRecommendations(@RequestAttribute Claims claims, @RequestBody Book book){
+		System.out.println(claims.getId());
 		try {
-			return new ResponseEntity<Book>(recommendationService.addtoRecommendations(book, userId), HttpStatus.OK);
+			return new ResponseEntity<Book>(recommendationService.addtoRecommendations(book, claims.getId()), HttpStatus.OK);
 		}catch (BookAlreadyExistsException e) {
 			return new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
 		}
 	}
 	
 	@DeleteMapping
-	public ResponseEntity<?> removeBookByUser(@RequestParam String userId, @RequestBody Book book){
+	public ResponseEntity<?> removeBookByUser(@RequestAttribute Claims claims, @RequestParam String userId, @RequestBody Book book){
+		System.out.println(claims.getId());
 		try {
-			return new ResponseEntity<Book>(recommendationService.removeBookByUser(book, userId), HttpStatus.OK);
+			return new ResponseEntity<Book>(recommendationService.removeBookByUser(book, claims.getId()), HttpStatus.OK);
 		}catch (Exception e) {
 			return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
 		}
