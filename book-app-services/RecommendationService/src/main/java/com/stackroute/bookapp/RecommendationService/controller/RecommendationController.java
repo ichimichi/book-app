8cd28648f7704fb979534fde07b7f953b67fa8de
Laribok.Syiemlieh <laribok.syiemlieh@ibm.com>
2021-03-29 14:25:37 +0530

    allowed app through cors for all services

--- book-app-services/RecommendationService/src/main/java/com/stackroute/bookapp/RecommendationService/controller/RecommendationController.java
+++ book-app-services/RecommendationService/src/main/java/com/stackroute/bookapp/RecommendationService/controller/RecommendationController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.stackroute.bookapp.RecommendationService.model.Book;
 import com.stackroute.bookapp.RecommendationService.service.RecommendationServiceImpl;
 
 @RestController
+@CrossOrigin("http://localhost:4200")
 @RequestMapping("/api/v1/recommendation")
 public class RecommendationController {
 	
