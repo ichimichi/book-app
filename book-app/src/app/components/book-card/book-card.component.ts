d8d32a6f598d4af06cc7f925d1e065c2dd6a09e3
Laribok.Syiemlieh <laribok.syiemlieh@ibm.com>
2021-03-30 11:18:38 +0530

    implemented alert dialog

--- book-app/src/app/components/book-card/book-card.component.ts
+++ book-app/src/app/components/book-card/book-card.component.ts
@@ -1,7 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { Book } from 'src/app/models/book';
+import { DialogData } from 'src/app/models/dialog-data';
 import { ImageLinks } from 'src/app/models/image-links';
 import { RecommendationService } from 'src/app/services/recommendation.service';
+import { DialogAlertComponent } from '../dialog-alert/dialog-alert.component';
 
 @Component({
   selector: 'app-book-card',
@@ -10,7 +14,12 @@ import { RecommendationService } from 'src/app/services/recommendation.service';
 })
 export class BookCardComponent implements OnInit {
   @Input() book: Book | undefined;
-  constructor(private recommendationService: RecommendationService) {}
+  dialogData: DialogData | undefined;
+
+  constructor(
+    private recommendationService: RecommendationService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {}
 
@@ -30,9 +39,23 @@ export class BookCardComponent implements OnInit {
       },
       (err) => {
         console.error(err);
+        this.dialogData = {
+          title: 'Failed to recommend boook',
+          message: 'You need to be logged in to start recommending books',
+          description: '',
+          buttonText: ['Sign In', 'Later'],
+          redirect: ['/login'],
+        };
+        this.openDialog(this.dialogData);
       }
     );
   }
 
-  addToFavourite(book: Book) {}
+  addToFavourite(book: Book) {
+    console.log('hi');
+  }
+
+  openDialog(dialogData: DialogData) {
+    this.dialog.open(DialogAlertComponent, { data: dialogData });
+  }
 }
--- book-app/src/app/components/search-result/search-result.component.ts
+++ book-app/src/app/components/search-result/search-result.component.ts
@@ -1,4 +1,3 @@
-import { utf8Encode } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Book } from 'src/app/models/book';
