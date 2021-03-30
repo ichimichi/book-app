7e0790005550001b3d51b0028af4aa0e73bac347
Laribok.Syiemlieh <laribok.syiemlieh@ibm.com>
2021-03-30 17:18:15 +0530

    refactore auth guard

--- book-app/src/app/guard/auth.guard.ts
+++ book-app/src/app/guard/auth.guard.ts
@@ -21,17 +21,11 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
+  ): boolean {
     const isAuthenticated = this.useAuthService.isLoggedIn();
-    if (isAuthenticated) {
-      return isAuthenticated;
-    } else {
+    if (!isAuthenticated) {
       this.routeService.goto404();
-      return isAuthenticated;
     }
+    return isAuthenticated;
   }
 }
